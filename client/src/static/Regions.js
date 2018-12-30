ade0df5a0566bdef79d50308ba49b5339d0661af
Shane4666 <sc897@nau.edu>
2018-12-29 18:19:53 -0700

    regions added.

---
+++ client/src/static/Regions.js
@@ -0,0 +1,50 @@
+export const regions = [
+    {
+        "name": "North America",
+        "countryCode": "NA"
+    },
+    {
+        "name": "Brazil",
+        "countryCode": "BR1"
+    },
+    {
+        "name": "Europe Nordic & East",
+        "countryCode": "EUN1"
+    },
+    {
+        "name": "Europe West",
+        "countryCode": "EUW"
+    },
+    {
+        "name": "Japan",
+        "countryCode": "JP"
+    },
+    {
+        "name": "Korea",
+        "countryCode": "KR"
+    },
+    {
+        "name": "Latin America North",
+        "countryCode": "LA1"
+    },
+    {
+        "name": "Latin America South",
+        "countryCode": "LA2"
+    },
+    {
+        "name": "Oceania",
+        "countryCode": "OC1"
+    },
+    {
+        "name": "Turkey",
+        "countryCode": "TR1"
+    },
+    {
+        "name": "Russia",
+        "countryCode": "RU"
+    },
+    {
+        "name": "PBE",
+        "countryCode": "PBE1"
+    }
+];
