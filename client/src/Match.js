4d68db899ed472f5e4f63b77262813d8c55ae32a
Shane Cook <sc897@nau.edu>
2019-01-02 20:01:45 -0700

    prettierfied match.js

--- client/src/Match.js
+++ client/src/Match.js
@@ -15,19 +15,23 @@ class Match extends React.Component {
   };
 
   grabChamp = champKey => {
-    const champ= Object.values(this.state.championData.data);
-    const champion = champ.find(champion => champion.key === champKey.toString());
+    const champ = Object.values(this.state.championData.data);
+    const champion = champ.find(
+      champion => champion.key === champKey.toString()
+    );
     if (champion) {
       return champion.image.full;
     }
-    return 'Ashe.png';
+    return "Ashe.png";
   };
 
   render() {
     return (
       <div className="matchbox">
         <img
-          src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${this.grabChamp(this.state.matchData.champion)}`}
+          src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${this.grabChamp(
+            this.state.matchData.champion
+          )}`}
           alt="Champion Images"
         />
         <p>{this.convertTime(this.state.matchData.timestamp)}</p>
